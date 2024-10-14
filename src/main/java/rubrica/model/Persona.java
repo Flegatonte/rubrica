65e7deb4f43e25e53f005b7a1777db60e9d74590
Flegatonte <gloriafiammengo555@gmail.com>
2024-10-14 11:53:33 +0200

    Persona and Rubrica classes created

--- src/main/java/rubrica/model/Persona.java
+++ src/main/java/rubrica/model/Persona.java
@@ -1,4 +1,67 @@
 package rubrica.model;
 
 public class Persona {
+    private final long id;
+    private String nome;
+    private String cognome;
+    private String indirizzo;
+    private String telefono;
+    private int eta;
+
+    public Persona(long id, String nome, String cognome, String indirizzo, String telefono, int eta) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.indirizzo = indirizzo;
+        this.telefono = telefono;
+        this.eta = eta;
+    }
+
+    public boolean equals(Persona p) {
+        return this.id == p.getID();
+    }
+
+    public long getID() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
 }
--- src/main/java/rubrica/model/Rubrica.java
+++ src/main/java/rubrica/model/Rubrica.java
@@ -1,4 +1,47 @@
 package rubrica.model;
+import java.util.ArrayList;
+import java.util.Vector;
 
 public class Rubrica {
+    private Vector<Persona> rubrica;
+
+    public Rubrica(Vector<Persona> rubrica) {
+        this.rubrica = rubrica;
+    }
+
+    public Vector<Persona> getRubrica() {
+        return rubrica;
+    }
+
+    public void setRubrica(Vector<Persona> rubrica) {
+        this.rubrica = rubrica;
+    }
+
+    public void aggiungiPersona(Persona persona) {
+        rubrica.add(persona);
+    }
+
+    public Persona cercaPersonaPerID(long id) {
+        for (Persona p : rubrica) {
+            if (p.getID() == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public void modificaPersona(Persona p) {
+        rimuoviPersona(p.getID());
+        rubrica.add(p);
+    }
+
+    public void rimuoviPersona(long id) {
+        rubrica.removeIf(p -> p.getID() == (id));
+    }
+
+    public void stampaPersone() {
+        for (Persona p : rubrica) {
+            System.out.println(p);
+        }
+    }
 }
