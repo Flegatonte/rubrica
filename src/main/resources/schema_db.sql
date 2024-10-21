906b56a5c0db4bfa540f79f04a0b93dc59b020f3
Flegatonte <gloriafiammengo555@gmail.com>
2024-10-21 10:58:37 +0200

    Revert "Revert "controllers completed""
    This reverts commit a3fa37550a8eeca5abc6686e6cd395a57d04bbfb.

---
+++ src/main/resources/schema_db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE rubrica_db;
+
+USE rubrica_db;
+
+CREATE TABLE persone (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    cognome VARCHAR(50),
+    indirizzo VARCHAR(100),
+    telefono VARCHAR(20),
+    eta INT
+);
+
